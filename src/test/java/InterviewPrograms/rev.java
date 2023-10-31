038e55db00edfb39047c951d1ca07eadd1ed03f4
214koushi <koushikkushi1995@gmail.com>
2023-10-31 17:08:57 +0530

    java sanjay

---
+++ src/test/java/InterviewPrograms/rev.java
@@ -0,0 +1,14 @@
+package InterviewPrograms;
+
+import org.testng.annotations.Test;
+
+public class rev {
+
+	@Test
+	public void rever() {
+		String s="koushik";
+		for (int i = s.length()-1; i >=0; i--) {
+			System.out.println();
+		}
+	}
+}
